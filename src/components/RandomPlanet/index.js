b2402b400a0d73aa1112ce234c5a38b281db512e
egorkqq <egorkqq@gmail.com>
2018-12-17 15:55:38 +0500

    feat: some randomPlanet logic and refactoring api method (best practice)

--- src/components/RandomPlanet/index.js
+++ src/components/RandomPlanet/index.js
@@ -1,24 +1,52 @@
 import React, { Component } from "react";
+import SwapiService from "./../../services/swapi";
+import Spinner from "./../Loader";
 import "./styles.sass";
 class RandomPlanet extends Component {
-  state = {};
+  swapiService = new SwapiService();
+  state = {
+    planet: {},
+    isLoading: true
+  };
+  constructor() {
+    super();
+    this.updatePlanet();
+  }
+  onPlanetLoaded = planet => {
+    this.setState({ planet });
+  };
+  updatePlanet() {
+    const id = Math.floor(Math.random() * 25 + 2);
+    this.swapiService.getPlanet(id).then(this.onPlanetLoaded);
+  }
   render() {
+    const {
+      planet: { id, name, population, rotationPeriod, diameter }
+    } = this.state;
     return (
-      <div class="jumbotron jumbotron-fluid mt-4 p-0">
+      <div class="jumbotron rounded mt-4 p-0">
         <div class="random-planet">
-          <div className="planet-image embed-responsive-item">
+          <div className="planet-image ">
             <img
-              src="https://starwars.ru/media/cache/ce/1b/ce1bd0373e9ef1b3368773b1b6cb153c.png"
-              alt="Death Star"
+              alt="Planet"
+              src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
               width="100%"
             />
           </div>
           <div className="planet-info">
-            <h3>Star of the Death</h3>
+            <h3>{name}</h3>
             <ul class="list-group list-group-flush">
-              <li class="list-group-item">Cras justo odio</li>
-              <li class="list-group-item">Dapibus ac facilisis in</li>
-              <li class="list-group-item">Morbi leo risus</li>
+              <li class="list-group-item">
+                <span>Population:</span> <span> {population}</span>
+              </li>
+              <li class="list-group-item">
+                <span>Rotation Period: </span>
+                <span children={rotationPeriod} />
+              </li>
+              <li class="list-group-item">
+                <span children="Diameter: " />
+                <span children={diameter} />
+              </li>
             </ul>
           </div>
         </div>

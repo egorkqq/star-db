4a5e5db300ad69e610e52b1384beba5893b8883d
egorkqq <egorkqq@gmail.com>
2018-12-18 11:56:43 +0500

    refactor: itemlist and person details now have wrapper

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,24 +1,16 @@
 import React, { Component, Fragment } from "react";
 import Header from "./../Header";
 import RandomPlanet from "./../RandomPlanet";
-import PersonDetails from "./../PersonDetails";
-import ItemList from "./../ItemList";
+import PeoplePage from "./../PeoplePage";
 import "./styles.sass";
+import ErrorIndicator from "../ErrorIndicator";
 class App extends Component {
-  state = {};
   render() {
     return (
       <div className="container">
         <Header />
         <RandomPlanet />
-        <div className="row">
-          <div className="col-md-6">
-            <ItemList />
-          </div>
-          <div className="col-md-6">
-            <PersonDetails />
-          </div>
-        </div>
+        <PeoplePage />
       </div>
     );
   }

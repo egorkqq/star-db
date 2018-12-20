c65f236fef1d3c7f2bfa723540054596fa442f09
egorkqq <egorkqq@gmail.com>
2018-12-20 09:38:44 +0500

    Updates

---
+++ precache-manifest.259def3a12daae1b33a53198d9b1a254.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7ae2188884618d0aefdc764315bd3a33",
+    "url": "/stardb/static/media/image.7ae21888.png"
+  },
+  {
+    "revision": "e2dba1e2e722e3a72ca8",
+    "url": "/stardb/static/js/runtime~main.e2dba1e2.js"
+  },
+  {
+    "revision": "674bfc7818942e2be71d",
+    "url": "/stardb/static/js/main.674bfc78.chunk.js"
+  },
+  {
+    "revision": "4bd3e9520aad91322b4b",
+    "url": "/stardb/static/js/1.4bd3e952.chunk.js"
+  },
+  {
+    "revision": "674bfc7818942e2be71d",
+    "url": "/stardb/static/css/main.9a2a4276.chunk.css"
+  },
+  {
+    "revision": "2026643dd3c647cb7747eca92297d4cc",
+    "url": "/stardb/index.html"
+  }
+];

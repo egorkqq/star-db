090b076a9b984052af690e90416ee63060e7e93f
egorkqq <egorkqq@gmail.com>
2018-12-20 09:42:51 +0500

    Updates

---
+++ precache-manifest.7c10f59ca7c4d7d49104c04df2bc2465.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7ae2188884618d0aefdc764315bd3a33",
+    "url": "/star-db/static/media/image.7ae21888.png"
+  },
+  {
+    "revision": "efac5df8a606d470b452",
+    "url": "/star-db/static/js/runtime~main.efac5df8.js"
+  },
+  {
+    "revision": "674bfc7818942e2be71d",
+    "url": "/star-db/static/js/main.674bfc78.chunk.js"
+  },
+  {
+    "revision": "4bd3e9520aad91322b4b",
+    "url": "/star-db/static/js/1.4bd3e952.chunk.js"
+  },
+  {
+    "revision": "674bfc7818942e2be71d",
+    "url": "/star-db/static/css/main.9a2a4276.chunk.css"
+  },
+  {
+    "revision": "9f393414cc984a18042ce79cd4732987",
+    "url": "/star-db/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/stardb/precache-manifest.259def3a12daae1b33a53198d9b1a254.js"
+  "/star-db/precache-manifest.7c10f59ca7c4d7d49104c04df2bc2465.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/stardb/index.html", {
+workbox.routing.registerNavigationRoute("/star-db/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
